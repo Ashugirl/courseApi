8900e59b2ff8b37c9fd2376c88ae72ad86707810
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-08-31 08:19:49 +0200

    Initial commit

---
+++ src/main/java/be/intecbrussel/CourseApiApp.java
@@ -0,0 +1,11 @@
+package be.intecbrussel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
